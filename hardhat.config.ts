170047443bbc1ed3e38440a0be01583a34bfdba1
web3isthefuture <web3isthefuture@gmail.com>
2023-04-15 15:12:40 +0900

    add dependencies

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,3 +1,8 @@
+require("dotenv").config();
+require("@nomiclabs/hardhat-etherscan");
+require("@nomicfoundation/hardhat-chai-matchers");
+require('hardhat-abi-exporter');
+
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
