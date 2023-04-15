436606bfc33a31090195cdfcd160d09376b8e205
web3isthefuture <web3isthefuture@gmail.com>
2023-04-15 15:18:13 +0900

    add mantle network to config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -6,8 +6,26 @@ require('hardhat-abi-exporter');
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+const PRIVATE_KEY_1 = process.env.PRIVATE_KEY_1;
+const PRIVATE_KEY_2 = process.env.PRIVATE_KEY_2;
+
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
+  networks: {
+    bsc_testnet: {
+      url: process.env.BSC_TESTNET_RPC_URL,
+      accounts: [`${PRIVATE_KEY_1}`, `${PRIVATE_KEY_2}`],
+      gas : 10000000,
+      gasPrice : 20000000000,
+    },
+    mantle_testnet: {
+      url: process.env.MANTLE_TESTNET_RPC_URL,
+      accounts: [`${PRIVATE_KEY_1}`, `${PRIVATE_KEY_2}`],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
 };
 
 export default config;
